02f2d88179bd847fd400f3e72e67e74394142517
passioneffort <agronkercishta.ai@gmail.com>
2023-10-07 00:51:12 -0400

    add

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'helloworld-app';
+  message = 'This is my first Angular Application'
+}

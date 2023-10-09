135ecad006c6eda1d291e44609fa5d3e34f3c92e
passioneffort <agronkercishta.ai@gmail.com>
2023-10-09 08:36:12 -0400

    modify

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,18 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class EmployeeService {
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
 
-//   constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-//   addEmployee(data: any): Observable<any> {
-//     return this._http.post('http://localhost:3000/employees', data);
-//   }
+  addEmployee(data: any): Observable<any> {
+    return this._http.post('http://localhost:3000/employees', data);
+  }
 
-//   getEmployeeList(): Observable<any> {
-//     return this._http.get('http://localhost:3000/employees');
-//   }
-// }
+  getEmployeeList(): Observable<any> {
+    return this._http.get('http://localhost:3000/employees');
+  }
+}
